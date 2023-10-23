f28aec34cda19e98d620d7f30ede775f2d31a68c
Daria Leshchinskaya <daria.leshchinskaya@innowise-group.com>
2023-10-23 06:51:58 -0700

    add abstract Header navigation links logic

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 'use client'
 import styles from './Header.module.css'
 import Link from "next/link";
+import {INavigationLink} from "@/types/navigationLinks.types";
 
-export const Header = () => {
+export const Header = ({navigationLinks}: { navigationLinks: INavigationLink[] }) => {
     return <header className={styles.headerContainer}>
-        <Link href={'/'}>Characters</Link>
-        <Link href={"/about"}>About</Link>
+        {navigationLinks.map((link) => (
+            <Link key={link.title} className={styles.navLink} href={link.link}>{link.title}</Link>)
+        )}
     </header>
 }
