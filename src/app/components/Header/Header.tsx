36f50cfc26b94896adaff76321619b13a33fbc98
Daria Leshchinskaya <daria.leshchinskaya@innowise-group.com>
2023-10-23 05:06:22 -0700

    add logic of character cards

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,10 @@
+'use client'
 import styles from './Header.module.css'
 import Link from "next/link";
 
 export const Header = () => {
     return <header className={styles.headerContainer}>
-        <Link href={'/'}>Home</Link>
+        <Link href={'/'}>Characters</Link>
         <Link href={"/about"}>About</Link>
     </header>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 import styles from './page.module.css'
+import {ICharacter, IServerCharacter} from "@/types/character.types";
+import {CharacterCard} from "@/app/components/CharacterCard/CharacterCard";
 
-export default function Home() {
+async function getCharacters() {
+    const res = await fetch('https://rickandmortyapi.com/api/character')
+    const characters = await res.json()
+    return (characters.results as IServerCharacter[]).map(
+        (character) => (
+            {
+                name: character.name,
+                gender: character.gender,
+                status: character.status,
+                species: character.species,
+                imgSrc: character.image
+            }
+        )) as ICharacter[]
+}
+
+export default async function Characters() {
+    const characters = await getCharacters()
+
   return (
-      <div className={styles.description}>
-          Home
+      <div className={styles.charactersContainer}>
+          {characters.map(({name, gender, imgSrc, species, status}) => {
+              return <CharacterCard
+                  key={name}
+                  imgSrc={imgSrc}
+                  name={name}
+                  status={status}
+                  species={species}
+                  gender={gender}
+              />
+          })}
       </div>
   )
 }
